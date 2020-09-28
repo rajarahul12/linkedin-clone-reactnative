8e165deffbed71f849dc40290852e76bdae92c89
rajarahul12 <maddula.rajarahul@gmail.com>
2020-09-28 23:58:42 +0530

    Adding Activity Indicator

---
+++ app/screens/ActivityIndicator.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ActivityIndicator() {
+  return (
+    <View>
+      <Text>ActivityIndicator</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
